e4c0fab0ca9367d8ac5d29883b9aba02f949f449
NuongBee2003 <thanhdats22003@gmail.com>
2026-06-01 11:22:42 +0700

    refactor: remove NODE_ENV variable and database connection test from connectionDB.js

--- src/config/connectionDB.js
+++ src/config/connectionDB.js
@@ -21,16 +21,4 @@ const sequelize = new Sequelize(
     logging: false,
   }
 );
-const testConnetion = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
-
-if (process.env.NODE_ENV !== 'production') {
-  testConnetion();
-}
 module.exports = sequelize;
